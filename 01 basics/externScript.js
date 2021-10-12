b990427c64039cd20b2b1679610dd56e4a5942bf
abdultanveer <abdul.tanveer@gmail.com>
2021-10-12 14:17:02 +0530

    external
    external javascript

---
+++ 01 basics/externScript.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    console.log('myfunction starts')
+        var para =  document.getElementById('mypara2')
+        var text = document.createTextNode("abdul")
+        para.appendChild(text)
+    console.log('myfunction ends')
+
+    }
